31fca6f7d979d8f4dde6e56c35c7887548b7d136
john lin <jielinxiao@163.com>
2014-05-07 17:05:29 +0800

    add routines of exercise

--- chapter-4/chap4.c
+++ chapter-4/chap4.c
@@ -1,5 +1,137 @@
 
 /*
+ * reverse @s 
+ * use recursive
+ * not correct and good
+ */
+#include <stdio.h>
+char *__reverse_recursive(char s[], int low, int high)
+{
+	int 	i;
+	char	tmp;
+	int		mid;
+	static int count = 0;
+
+	printf("s: %s, count: %d\n", s, count);
+	mid = low + (high - low) / 2;
+	if (low < high) {
+		__reverse_recursive(s, low, mid - 1);
+		__reverse_recursive(s, mid + 1, high);
+	}
+
+	for (; mid < high; low++) {
+		tmp = s[low];
+		s[low] = s[++mid];
+		s[mid] = tmp;
+	}
+	printf("s: %s, count: %d\n", s, count++);
+
+	return s;
+}
+
+char *reverse_recursive(char s[])
+{
+	int len;
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	__reverse_recursive(s, 0, len - 1);
+	return s;
+}
+/*
+ * convert @num to string stored in @s
+ * use recursive 	
+ * hard to get the idea of using static variable
+ */
+#define private_abs(n) ((n) < 0 ? -(n) : (n))
+char *itoa_recursive(int num, char s[])
+{
+	static int i = 0;
+
+	if (num / 10) {
+		itoa_recursive(num / 10, s);
+	} else {
+		if (num < 0) {
+			s[i++] = '-';;
+			num = -num;
+		}
+	}
+	s[i++] = private_abs(num) % 10 + '0';
+	s[i] = '\0';
+
+	return s;
+	/*
+	if (num < 10) {
+		s[0] = "0123456789"[num];
+		s[1] = '\0';
+	} else {
+		s[0] = "0123456789"[num % 10];
+		itoa_recursive(num / 10, &s[1]);
+	}
+	return s;
+	*/
+}
+
+/*
+ * getch ungetch :  buffered one char
+ */
+#include <stdio.h>	// for fprintf getchar
+static int tmp_buf;
+int getch()
+{
+	int c;
+
+	if (tmp_buf != 0) {
+		c = tmp_buf;
+		tmp_buf = 0;
+	} else
+		c = getchar();
+	return c;
+}
+
+void ungetch(int c)
+{
+	if (tmp_buf == 0)
+		tmp_buf = c;
+	else
+		fprintf(stderr, "only support ungetch one char\n");
+}
+
+/*
+ * second version of getch and ungetch, which can store BUF_SIZE chars
+ */
+#define BUF_SIZE	1024
+static int tmp_buf_l[1024];
+static int tmp_bufp = 0;
+int getch_l()
+{
+	return (tmp_bufp > 0) ? tmp_buf_l[--tmp_bufp] : getchar();
+}
+void ungetch_l(int c)
+{
+	if (tmp_bufp >= BUF_SIZE)
+		fprintf(stderr, "too many chars to unget, max: %d\n", BUF_SIZE);
+	else
+		tmp_buf_l[tmp_bufp++] = c;
+}
+
+
+/*
+ * push string @s back on to input
+ */
+#include <stdio.h>	// stdin and ungetc
+#define ungetch(c) ungetc(c, stdin)
+void ungets(char s[])
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	while (len-- > 0) 
+		ungetch(s[len]);
+}
+
+/*
  * convert string to double
  * can deal with ***e(+-)** value
  */
@@ -141,14 +273,15 @@ int strrindex_fast(char s[], char t[])
 int main(int argc, char *argv[])
 {
 	char *p = "123.456";
-	if (argc != 3) {
-		printf("usage: %s double double\n", argv[0]);
+	char	s[1024] = "hello  world";
+	int 	value = 123;
+
+	if (argc != 1) {
+		printf("usage: %s int\n", argv[0]);
 		exit(1);
 	}
-	printf("str: %s value: %f\n", p, atof_prv(p));
-	printf("str: %s value: %f\n", argv[1], atof_prv(argv[1]));
-	printf("str: %s value: %f\n", argv[1], atof_c(argv[1]));
-	printf("str: %s value: %f\n", argv[2], atof_c(argv[2]));
+	printf("str: %s\n", s);
+	printf("reverse: %s\n", reverse_recursive(s));
 	exit(0);
 }
 
